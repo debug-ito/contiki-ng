1d74b84eb51b98ba0c69483b9434fb573cf9a774
Toshio Ito <debug.ito@gmail.com>
2019-10-12 20:15:18 +0900

    mpmc-ring: save get_ptr and put_ptr temporarily.
    because get_ptr and put_ptr can move without noticing. it's dangerous.

--- os/lib/mpmc-ring.c
+++ os/lib/mpmc-ring.c
@@ -13,9 +13,11 @@ int
 mpmc_ring_put_start(struct mpmc_ring *ring)
 {
   uint8_t tmp_put;
+  uint8_t now_get;
   assert(ring != NULL);
+  now_get = ring->get_ptr;
   for(tmp_put = ring->put_ptr;
-      ((tmp_put - ring->get_ptr) & MPMC_RING_MASK) != MPMC_RING_MASK;
+      ((tmp_put - now_get) & MPMC_RING_MASK) != MPMC_RING_MASK;
       tmp_put = (tmp_put + 1) & MPMC_RING_MASK) {
     if(ring->state[tmp_put] == MPMC_RING_EMPTY &&
        atomic_cas_uint8(&ring->state[tmp_put], MPMC_RING_EMPTY, MPMC_RING_PUTTING)) {
@@ -43,9 +45,11 @@ int
 mpmc_ring_get_start(struct mpmc_ring *ring)
 {
   uint8_t tmp_get;
+  uint8_t now_put;
   assert(ring != NULL);
+  now_put = ring->put_ptr;
   for(tmp_get = ring->get_ptr;
-      ((ring->put_ptr - tmp_get) & MPMC_RING_MASK) != 0;
+      ((now_put - tmp_get) & MPMC_RING_MASK) != 0;
       tmp_get = (tmp_get + 1) & MPMC_RING_MASK) {
     if(ring->state[tmp_get] == MPMC_RING_OCCUPIED &&
        atomic_cas_uint8(&ring->state[tmp_get], MPMC_RING_OCCUPIED, MPMC_RING_GETTING)) {
