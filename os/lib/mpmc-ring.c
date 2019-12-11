e8bfbb5dfd3fe7dd35c61f65a050b3ca7455e02a
Toshio Ito <toshio9.ito@toshiba.co.jp>
2019-12-11 10:10:29 +0900

    add os/lib/mpmc-ring

---
+++ os/lib/mpmc-ring.c
@@ -0,0 +1,268 @@
+#include <string.h>
+#include "lib/assert.h"
+#include "sys/memory-barrier.h"
+#include "sys/critical.h"
+
+#include "mpmc-ring.h"
+
+/*----------------------------------------------------------------------------------------*/
+/**
+ * State of a slot in the queue. Stored in ring->state array.
+ */
+enum mpmc_ring_state {
+  MPMC_RING_EMPTY = 0, /**< The slot is empty. Transitions to PUTTING by put_begin. */
+  MPMC_RING_PUTTING,   /**< A producer is writing to the slot right now. Transitions to OCCUPIED by put_commit. */
+  MPMC_RING_GETTING,   /**< A consumer is reading the slot right now. Transitions to EMPTY by get_commit. */
+  MPMC_RING_OCCUPIED,  /**< The slot is occupied. Transitions to GETTING by get_begin. */
+};
+
+/*
+ * Internal of mpmc_ring
+ * 
+ * The figure below shows an example of the `state` array of
+ * mpmc_ring.
+ *
+ *      0 1 2 3 4 5 6 7 8 9 a b c d e f
+ *     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *     |G|G|E|O|O|O|O|O|P|P|P|O|E|E|E|E|
+ *     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *      ^               ^
+ *      get_ptr         put_ptr
+ *
+ * The state array is divided into two regions; the O region
+ * (get_ptr <= i < put_ptr) and the E region (put_ptr <= i < get_ptr).
+ * 
+ * Slots in the O region are basically OCCUPIED, can be GETTING or
+ * EMPTY temporarily but never PUTTING. Slots in the E region are
+ * basically EMPTY, can be PUTTING or OCCUPIED temporarily but never
+ * GETTING. If get_ptr == put_ptr, the whole queue is in E region
+ * and there is no O region.
+ *
+ * get_ptr points to the lowest GETTING slot if there is any GETTING
+ * slots in the queue. Otherwise, get_ptr points to the lowest
+ * OCCUPIED slot if there is any OCCUPIED slots in the
+ * queue. Otherwise, get_ptr must be equal to put_ptr.
+ *
+ * put_ptr points to the lowest PUTTING slot if there is any PUTTING
+ * slots in the queue. Otherwise, put_ptr points to the lowest EMPTY
+ * slot. Note that there is always at least one EMPTY slot in the
+ * queue. So, if get_ptr == put_ptr, this means the queue is
+ * (almost) empty.
+ *
+ * GETTING slots can transition to EMPTY in out-of-order
+ * fashion. For example, the slot 2 in the above figure must have
+ * transitioned to GETTING after slots 0 and 1 became GETTING. Then,
+ * the slot 2 transitioned to EMPTY before slots 0 and 1. After
+ * slots 0 and 1 both transition to EMPTY, get_ptr will move to slot
+ * 3. Similar discussion applies to PUTTING slots and put_ptr.
+ *
+ */
+
+/*----------------------------------------------------------------------------------------*/
+
+static inline mpmc_ring_index_t
+elems(mpmc_ring_index_t put_index, mpmc_ring_index_t get_index, mpmc_ring_index_t mask)
+{
+  return (put_index - get_index) & mask;
+}
+
+static inline bool
+is_empty(mpmc_ring_index_t put_index, mpmc_ring_index_t get_index, mpmc_ring_index_t mask)
+{
+  return elems(put_index, get_index, mask) == 0;
+}
+
+static inline bool
+is_full(mpmc_ring_index_t put_index, mpmc_ring_index_t get_index, mpmc_ring_index_t mask)
+{
+  return elems(put_index, get_index, mask) == mask;
+}
+
+static inline mpmc_ring_index_t
+next(mpmc_ring_index_t i, mpmc_ring_index_t mask)
+{
+  return (i + 1) & mask;
+}
+
+/*----------------------------------------------------------------------------------------*/
+
+#if MPMC_RING_DEBUG_TRACE_ENABLED
+static inline void
+add_debug_trace(struct mpmc_ring *ring, mpmc_ring_index_t index, uint8_t event)
+{
+  int_master_status_t s = critical_enter();
+  ring->debug_trace[ring->debug_trace_next_put].target = index;
+  ring->debug_trace[ring->debug_trace_next_put].event = event;
+  ring->debug_trace_next_put = (ring->debug_trace_next_put + 1) % MPMC_RING_DEBUG_TRACE_SIZE;
+  critical_exit(s);
+}
+
+static inline uint16_t
+prev_trace_index(uint16_t index)
+{
+  return index == 0 ? (MPMC_RING_DEBUG_TRACE_SIZE - 1) : (index - 1);
+}
+
+static const char *trace_event_desc[] =
+  {
+    "UNDEFINED",
+    "EMPTY_TO_PUTTING",
+    "PUTTING_TO_OCCUPIED",
+    "OCCUPIED_TO_GETTING",
+    "GETTING_TO_EMPTY",
+    "NEXT_PUT_PTR",
+    "NEXT_GET_PTR",
+  };
+
+void
+mpmc_ring_print_debug_trace(const struct mpmc_ring *ring)
+{
+  uint16_t end = ring->debug_trace_next_put;
+  uint16_t i;
+  uint16_t n = 0;
+  for(i = prev_trace_index(end) ; i != end ; i = prev_trace_index(i), n++) {
+    const struct mpmc_ring_trace_entry *e = &ring->debug_trace[i];
+    if(e->event == MPMC_RING_TRACE_EVENT_UNDEFINED) break;
+    printf("[MPMC_RING_TRACE] %5u target:%-3u event:%u(%s)\n",
+           n, e->target, e->event, trace_event_desc[e->event]);
+  }
+}
+#else /* MPMC_RING_DEBUG_TRACE_ENABLED */
+#define add_debug_trace(r,i,e)
+#endif /* MPMC_RING_DEBUG_TRACE_ENABLED */
+
+/*----------------------------------------------------------------------------------------*/
+
+void
+mpmc_ring_init(struct mpmc_ring *ring)
+{
+  ring->put_ptr = 0;
+  ring->get_ptr = 0;
+  memset(ring->state, 0, (size_t)ring->mask + 1);
+#if MPMC_RING_DEBUG_TRACE_ENABLED
+  memset(ring->debug_trace, 0, sizeof(ring->debug_trace));
+  ring->debug_trace_next_put = 0;
+#endif /* MPMC_RING_DEBUG_TRACE_ENABLED */
+}
+
+int
+mpmc_ring_put_begin(struct mpmc_ring *ring)
+{
+  mpmc_ring_index_t tmp_put;
+  mpmc_ring_index_t now_get;
+  assert(ring != NULL);
+  now_get = ring->get_ptr;
+  tmp_put = ring->put_ptr;
+  memory_barrier();
+  while(!is_full(tmp_put, now_get, ring->mask)) {
+    if(atomic_cas_uint8(&ring->state[tmp_put], MPMC_RING_EMPTY, MPMC_RING_PUTTING)) {
+      add_debug_trace(ring, tmp_put, MPMC_RING_TRACE_EVENT_EMPTY_TO_PUTTING);
+      return tmp_put;
+    } else {
+      /* Failed to update the state */
+      memory_barrier();
+      if(ring->state[tmp_put] == MPMC_RING_EMPTY) {
+        /*
+         * Looks like no one succeeded to update the state. Retry at
+         * the same index.
+         */
+        ;
+      } else {
+        /*
+         * It's not EMPTY in the first place, or someone else
+         * updated the state. Try the next index.
+         */
+        tmp_put = next(tmp_put, ring->mask);
+      }
+    }
+  }
+  return -1;
+}
+
+void
+mpmc_ring_put_commit(struct mpmc_ring *ring, mpmc_ring_index_t index)
+{
+  mpmc_ring_index_t now_get;
+  assert(ring != NULL);
+  assert(ring->state[index] == MPMC_RING_PUTTING);
+  
+  ring->state[index] = MPMC_RING_OCCUPIED;
+  add_debug_trace(ring, index, MPMC_RING_TRACE_EVENT_PUTTING_TO_OCCUPIED);
+  now_get = ring->get_ptr;
+  while(1) {
+    mpmc_ring_index_t tmp_put;
+    tmp_put = ring->put_ptr;
+    memory_barrier();
+    if(ring->state[tmp_put] == MPMC_RING_OCCUPIED && !is_full(tmp_put, now_get, ring->mask)) {
+      if(atomic_cas_uint8(&ring->put_ptr, tmp_put, next(tmp_put, ring->mask))) {
+        add_debug_trace(ring, tmp_put, MPMC_RING_TRACE_EVENT_NEXT_PUT_PTR);
+      }
+    } else {
+      break;
+    }
+  }
+}
+
+int
+mpmc_ring_get_begin(struct mpmc_ring *ring)
+{
+  /* The dual of mpmc_ring_put_begin. */
+  mpmc_ring_index_t tmp_get;
+  mpmc_ring_index_t now_put;
+  assert(ring != NULL);
+  now_put = ring->put_ptr;
+  tmp_get = ring->get_ptr;
+  memory_barrier();
+  while(!is_empty(now_put, tmp_get, ring->mask)) {
+    if(atomic_cas_uint8(&ring->state[tmp_get], MPMC_RING_OCCUPIED, MPMC_RING_GETTING)) {
+      add_debug_trace(ring, tmp_get, MPMC_RING_TRACE_EVENT_OCCUPIED_TO_GETTING);
+      return tmp_get;
+    } else {
+      memory_barrier();
+      if(ring->state[tmp_get] == MPMC_RING_OCCUPIED) {
+        ;
+      } else {
+        tmp_get = next(tmp_get, ring->mask);
+      }
+    }
+  }
+  return -1;
+}
+
+void
+mpmc_ring_get_commit(struct mpmc_ring *ring, mpmc_ring_index_t index)
+{
+  /* The dual of mpmc_ring_put_commit. */
+  mpmc_ring_index_t now_put;
+  assert(ring != NULL);
+  assert(ring->state[index] == MPMC_RING_GETTING);
+  
+  ring->state[index] = MPMC_RING_EMPTY;
+  add_debug_trace(ring, index, MPMC_RING_TRACE_EVENT_GETTING_TO_EMPTY);
+  now_put = ring->put_ptr;
+  while(1) {
+    mpmc_ring_index_t tmp_get;
+    tmp_get = ring->get_ptr;
+    memory_barrier();
+    if(ring->state[tmp_get] == MPMC_RING_EMPTY && !is_empty(now_put, tmp_get, ring->mask)) {
+      if(atomic_cas_uint8(&ring->get_ptr, tmp_get, next(tmp_get, ring->mask))) {
+        add_debug_trace(ring, tmp_get, MPMC_RING_TRACE_EVENT_NEXT_GET_PTR);
+      }
+    } else {
+      break;
+    }
+  }
+}
+
+int
+mpmc_ring_elements(const struct mpmc_ring *ring)
+{
+  assert(ring != NULL);
+  return elems(ring->put_ptr, ring->get_ptr, ring->mask);
+}
+
+int
+mpmc_ring_empty(const struct mpmc_ring *ring)
+{
+  return mpmc_ring_elements(ring) == 0;
+}
---
+++ os/lib/mpmc-ring.h
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2019, Toshiba Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * \file
+ *         Multi-Producer Multi-Consumer lock-free ring buffer
+ * \author
+ *         Toshio Ito <toshio9.ito@toshiba.co.jp>
+ *
+ * mpmc-ring is analogous to ringbufindex, but it supports
+ * multi-producer, multi-consumer scenarios. To do that, mpmc-ring
+ * requires extra memory.
+ *
+ * mpmc-ring uses sys/atomic to arbitrate parallel accesses to the
+ * queue. If your platform implements sys/atomic by deferring
+ * interrupts, mpmc-queue also defers interrupts.
+ *
+ * To define and allocate a mpmc-ring object, use MPMC_RING macro.
+ */
+
+#ifndef _MPMC_RING_H_
+#define _MPMC_RING_H_
+
+#include "contiki.h"
+#include "sys/atomic.h"
+#include "sys/cc.h"
+
+/*----------------------------------------------------------------------------------------*/
+
+#ifdef MPMC_RING_CONF_DEBUG_TRACE_SIZE
+#define MPMC_RING_DEBUG_TRACE_SIZE MPMC_RING_CONF_DEBUG_TRACE_SIZE
+#else /* MPMC_RING_CONF_DEBUG_TRACE_SIZE */
+#define MPMC_RING_DEBUG_TRACE_SIZE 0
+#endif /* MPMC_RING_CONF_DEBUG_TRACE_SIZE */
+
+#define MPMC_RING_DEBUG_TRACE_ENABLED ((MPMC_RING_DEBUG_TRACE_SIZE) > 0)
+
+/*----------------------------------------------------------------------------------------*/
+
+/**
+ * Index managed by a mpmc_ring.
+ */
+typedef uint8_t mpmc_ring_index_t;
+
+#if MPMC_RING_DEBUG_TRACE_ENABLED
+enum mpmc_ring_trace_event {
+  MPMC_RING_TRACE_EVENT_UNDEFINED = 0,
+  MPMC_RING_TRACE_EVENT_EMPTY_TO_PUTTING,
+  MPMC_RING_TRACE_EVENT_PUTTING_TO_OCCUPIED,
+  MPMC_RING_TRACE_EVENT_OCCUPIED_TO_GETTING,
+  MPMC_RING_TRACE_EVENT_GETTING_TO_EMPTY,
+  MPMC_RING_TRACE_EVENT_NEXT_PUT_PTR,
+  MPMC_RING_TRACE_EVENT_NEXT_GET_PTR,
+};
+
+/**
+ * An entry of trace of behavior of mpmc_queue, used for debug.
+ */
+struct mpmc_ring_trace_entry {
+  mpmc_ring_index_t target;
+  uint8_t event;
+};
+#endif /* MPMC_RING_DEBUG_TRACE_ENABLED */
+
+
+/**
+ * The multi-producer multi-consumer ring buffer.
+ *
+ * Do not declare and define struct mpmc_ring directly. Use
+ * MPMC_RING macro instead.
+ */
+struct mpmc_ring {
+  mpmc_ring_index_t put_ptr;
+  mpmc_ring_index_t get_ptr;
+  uint8_t * const state;
+  const mpmc_ring_index_t mask;
+#if MPMC_RING_DEBUG_TRACE_ENABLED
+  struct mpmc_ring_trace_entry debug_trace[MPMC_RING_DEBUG_TRACE_SIZE];
+  uint16_t debug_trace_next_put;
+#endif /* MPMC_RING_DEBUG_TRACE_ENABLED */
+};
+
+/*----------------------------------------------------------------------------------------*/
+
+/**
+ * Declare and define a mpmc_ring.
+ *
+ * \param name Variable name of the struct mpmc_ring.
+ *
+ * \param size Size of the array that keeps the queue elements. Must
+ * be power of 2.
+ */
+#define MPMC_RING(name, size)                                           \
+  static uint8_t CC_CONCAT(name,_state)[size];                          \
+  static struct mpmc_ring name = { 0, 0, CC_CONCAT(name,_state), (size) - 1 };
+
+/**
+ * Initialize the mpmc_ring.
+ */
+void mpmc_ring_init(struct mpmc_ring *ring);
+
+/**
+ * Start putting an element to the queue. Every call to
+ * mpmc_ring_put_begin must be finished by one and only call to
+ * mpmc_ring_put_commit.
+ *
+ * \retval >=0 The index that the caller should use to put an
+ * element.
+ *
+ * \retval <0 The queue is full. The caller cannot put an element.
+ */
+int mpmc_ring_put_begin(struct mpmc_ring *ring);
+
+/**
+ * Finish putting an element to the queue.
+ *
+ * \param index The index obtained by the matching call to
+ * mpmc_ring_put_begin.
+ *
+ */
+void mpmc_ring_put_commit(struct mpmc_ring *ring, mpmc_ring_index_t index);
+
+/**
+ * Start getting an element from the queue. Every call to
+ * mpmc_ring_get_begin must be finished by one and only call to
+ * mpmc_ring_get_commit.
+ *
+ * \retval >=0 The index that the caller should use to get an
+ * element.
+ *
+ * \retval <0 The queue is empty. The caller cannot get an element.
+ */
+int mpmc_ring_get_begin(struct mpmc_ring *ring);
+
+/**
+ * Finish getting an element to the queue.
+ *
+ * \param index The index obtained by the matching call to
+ * mpmc_ring_get_begin.
+ *
+ */
+void mpmc_ring_get_commit(struct mpmc_ring *ring, mpmc_ring_index_t index);
+
+/**
+ * \return Number of elements currently in the queue.
+ */
+int mpmc_ring_elements(const struct mpmc_ring *ring);
+
+/**
+ * \return Non-zero if the queue is empty. Zero otherwise.
+ */
+int mpmc_ring_empty(const struct mpmc_ring *ring);
+
+#if MPMC_RING_DEBUG_TRACE_ENABLED
+void mpmc_ring_print_debug_trace(const struct mpmc_ring *ring);
+#else /* MPMC_RING_DEBUG_TRACE_ENABLED */
+#define mpmp_ring_print_debug_trace(r)
+#endif /* MPMC_RING_DEBUG_TRACE_ENABLED */
+
+
+#endif /* _MPMC_RING_H_ */
